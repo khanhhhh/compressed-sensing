dfdecbe14d87f3a85450b46148601c7db1dd7f31
khanh <nguyenngockhanh.pbc@gmail.com>
2021-09-03 16:10:24 +0800

    updated

--- example/compressed_sensing/measure.py
+++ example/compressed_sensing/measure.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 
-def create_measure_matrix(D: int, N: int) -> np.ndarray:
+def create_measure_matrix(d: int, n: int) -> np.ndarray:
     """
-    create_sensing_matrix
-    :param D: number of measure signal data points
-    :param N: number of true signal data points
-    :return: sensing matrix
+    create_measure_matrix
+    :param d: number of measure signal data points
+    :param n: number of true signal data points
+    :return: measure matrix
     """
-    M = np.zeros(shape=(D, N), dtype=np.int64)
-    indices = np.random.choice(np.arange(N), size=D, replace=False)
-    for d in range(D):
-        M[d, indices[d]] = 1
+    M = np.zeros(shape=(d, n), dtype=np.int64)
+    indices = np.random.choice(np.arange(n), size=d, replace=False)
+    for i in range(d):
+        M[i, indices[i]] = 1
     return M
